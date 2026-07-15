764a613181b4aff2edbbbb54b25259623c1352e3
Aaron David Chi Uc <al076228@uacam.mx>
2026-07-15 15:06:02 -0600

    El perrito corre mas rapido

--- Proyecto-Infomatrix/Godot-Engine/Scenes/Mini_Juego_Mesero/perrito.gd
+++ Proyecto-Infomatrix/Godot-Engine/Scenes/Mini_Juego_Mesero/perrito.gd
@@ -1,13 +1,27 @@
 extends Area2D
 
 @export var velocidad = 100
-@export var limite_izquierdo = 100
-@export var limite_derecho = 500
+@export var limite_izquierdo = 0
+@export var limite_derecho = 1152
 
 var direccion = 1 # 1 es derecha, -1 es izquierda
 
+# --- NUEVA VARIABLE ---
+# Este cronómetro acumula los segundos para saber cuándo cambiar de ritmo
+var tiempo_para_acelerar = 0.0
+
 func _process(delta):
-	# Movemos al perrito en el eje X
+	# --- NUEVO COMPORTAMIENTO IMPREDECIBLE ---
+	# Sumamos el tiempo que pasa en cada fotograma
+	tiempo_para_acelerar += delta
+	
+	# Cada 1.5 segundos, el perrito decide cambiar su velocidad por sorpresa
+	if tiempo_para_acelerar > 1.5:
+		# randf_range elige un número al azar entre un trote lento (100) y una carrera rápida (350)
+		velocidad = randf_range(150, 500)
+		tiempo_para_acelerar = 0.0 # Reiniciamos el cronómetro
+	
+	# Movemos al perrito en el eje X (usa la velocidad cambiante)
 	position.x += velocidad * direccion * delta
 	
 	# Reproducimos la animación correcta según a dónde va
