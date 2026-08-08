13f0cd7ba6e89f0f112c55e822ea1a180aeddfae
Aldair-Canul <al075462@uacam.mx>
2026-08-07 18:48:25 -0600

    Ajustes de importación de Godot y script contadores

--- Proyecto-Infomatrix/Godot-Engine/Scenes/Juego_Cuarto/contadores.gd
+++ Proyecto-Infomatrix/Godot-Engine/Scenes/Juego_Cuarto/contadores.gd
@@ -15,13 +15,11 @@ func _on_reloj_timeout():
 	tiempo_aumento += 1
 	texto_sube.text = str(tiempo_aumento)
 	
-	# 2. Lógica del contador que REDUCE
 	if tiempo_reduccion > 0:
 		tiempo_reduccion -= 1
 		texto_baja.text = str(tiempo_reduccion)
 		
-	# 3. ¿Qué pasa cuando el tiempo llega a cero?
+
 	if tiempo_reduccion == 0:
-		# ¡Apagamos el reloj para que no siga contando!
-		$Reloj.stop() 
+		$Reloj.stop()       
 		get_tree().change_scene_to_file("res://Scenes/Juego_Cuarto/final_cuarto.tscn")
